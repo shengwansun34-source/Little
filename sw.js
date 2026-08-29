2c53d2a8fe1a9ffdd032d42dd681dc87d936effb
shengwansun34-source <shengwansun34@gmail.com>
2026-08-30 02:25:59 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,39 @@
-const CACHE_NAME = 'little-v1.3.4';
-const URLS = ['./', './index.html', './style.css', 'js/db.js','js/state.js','js/home.js','js/memory.js','js/chat.js','js/settings.js',, './manifest.json'];
+const CACHE_NAME = 'little-v2.0.0';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './js/db.js',
+  './js/state.js',
+  './js/home.js',
+  './js/memory.js',
+  './js/chat.js',
+  './js/settings.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-180.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS)).then(() => self.skipWaiting()));
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  e.waitUntil(caches.keys().then(names => Promise.all(
+    names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))
+  )));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(cached => {
-    const net = fetch(e.request).then(res => {
-      if (res && res.status === 200) { const cl = res.clone(); caches.open(CACHE_NAME).then(c => c.put(e.request, cl)); }
+  e.respondWith(
+    fetch(e.request).then(res => {
+      if (res && res.status === 200) {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      }
       return res;
-    }).catch(() => cached);
-    return cached || net;
-  }));
+    }).catch(() => caches.match(e.request))
+  );
 });
