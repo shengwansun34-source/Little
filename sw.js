f0bd3ac539759344fa67af92f6ff1caadec33472
shengwansun34-source <shengwansun34@gmail.com>
2026-08-29 23:19:39 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'little-v1.3.0';
+const CACHE_NAME = 'little-v1.3.1';
 const URLS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
